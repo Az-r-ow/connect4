c713e0d9f1a3270cc388a250f8624c44c5eea708
Az-r-ow <antoine.azar123@gmail.com>
2023-04-16 23:54:59 +0200

    [FIXED] view.c file - now working coming_soon_view & help_view

--- src/views.c
+++ src/views.c
@@ -5,48 +5,77 @@
 #include <math.h>
 #include "views.h"
 
+char menu_options[5][50] = {
+    "2 Players",
+    "Coming Soon...",
+    "Help",
+    "Exit"};
+
+char help_title[] = "Welcome to Terminal Connect 4";
+
+char help_body[20][500] = {
+    "This is a two player game and hopefully one day you will be able to play against an AI.",
+    "But for now, this is how you play :"};
+
 WINDOW *main_window = NULL;
 
-void coming_soon_view(int row, int col)
+int user_choice = 0;
+
+// Window references
+int row, col, n_rows, n_cols, begin_y, begin_x, c_row, c_col;
+
+void coming_soon_view()
 {
   char comingsoontext[] = "Coming Soon...";
 
   wclear(main_window);
   wrefresh(main_window);
-  mvwprintw(main_window, (row / 2), (col / 2) - (strlen(comingsoontext) / 2), "%s", comingsoontext);
+
+  mvwprintw(main_window, c_row, c_col - (strlen(comingsoontext) / 2), "%s", comingsoontext);
 }
 
-void help_view(int row, int col)
+void help_view()
 {
   wclear(main_window);
   wrefresh(main_window);
-  char help_title[] = "Welcome to Terminal Connect 4";
 
-  char help_body[20][500] = {
-      "This is a two player game and hopefully one day you will be able to play against an AI.",
-      "But for now, this is how you play :"};
+  // center col
+  int print_col = c_col - (strlen(help_title) / 2);
 
-  mvwprintw(main_window, round(row * 0.11) + 1, (col / 2) - (strlen(help_title) / 2), "%s", help_title);
+  mvwprintw(main_window, (begin_y + 1), print_col, "%s", help_title);
   for (int i = 0; i < 2; i++)
   {
-    mvwprintw(main_window, round(row * 0.11) + 3 + i, round(col * 0.09), "%s", help_body[i]);
+    mvwprintw(main_window, begin_y + 3 + i, round(col * 0.09), "%s", help_body[i]);
   }
 }
 
 // Create the main menu and return user game type
 int main_menu_view()
 {
-  int row, col, w_row, w_col;
-
-  initscr();
-  refresh();
-
   getmaxyx(stdscr, row, col);
 
-  w_row = round(row * 0.8);
-  w_col = round(col * 0.8);
+  // Number of rows and cols
+  n_rows = round(row * 0.8);
+  n_cols = round(col * 0.8);
 
-  main_window = newwin(w_row, w_col, round((row * 0.11)), round((row * 0.33)));
+  // Starting coordinates
+  begin_y = round(row * 0.11);
+  begin_x = round(row * 0.33);
+
+  // Center row and col
+  c_row = round((row / 2) * 0.8);
+  c_col = round((col / 2) * 0.8);
+
+  // Only if user no user choice
+  if (!user_choice)
+  {
+    // Initialize screen
+    initscr();
+    refresh();
+
+    // initialise new window
+    main_window = newwin(n_rows, n_cols, begin_y, begin_x);
+  }
 
   // First check if the window size
   if (row <= 25 || col <= 50)
@@ -59,20 +88,11 @@ int main_menu_view()
 
   box(main_window, 0, 0);
 
-  char options[5][50] = {
-      "2 Players",
-      "Coming Soon...",
-      "Help",
-      "Exit"};
-
   // Number of options
-  int optionslen = (int)(sizeof(options) / sizeof(options[0]));
+  int menu_options_len = (int)(sizeof(menu_options) / sizeof(menu_options[0]));
 
   // The separation between each item
   int spacing = 2;
-
-  // The selected element
-  int selected = 0; // The first option on the list
 
   // The user's choice
   int choice;
@@ -82,33 +102,30 @@ int main_menu_view()
 
   while (1)
   {
-    for (int i = 0; i < optionslen; i++)
+    for (int i = 0; i < menu_options_len; i++)
     {
-      if (i == selected)
+      if (i == user_choice)
         wattron(main_window, A_REVERSE);
-      mvwprintw(main_window, 0, spacing, options[i]);
+      mvwprintw(main_window, 0, spacing, menu_options[i]);
       wattroff(main_window, A_REVERSE);
-      spacing += strlen(options[i]) + 2;
+      spacing += strlen(menu_options[i]) + 2;
     }
     // Reset the spacing
     spacing = 2;
 
     choice = wgetch(main_window);
 
-    // Invisible cursor
-    // curs_set(0);
-
     switch (choice)
     {
     case KEY_RIGHT:
-      if (selected == (optionslen - 2))
+      if (user_choice == (menu_options_len - 2))
         break;
-      selected++;
+      user_choice++;
       break;
     case KEY_LEFT:
-      if (selected == 0)
+      if (user_choice == 0)
         break;
-      selected--;
+      user_choice--;
       break;
     default:
       break;
@@ -117,20 +134,21 @@ int main_menu_view()
     // On ENTER
     if (choice == 10)
     {
-      switch (selected)
+      switch (user_choice)
       {
       case 0:
         // Start the game
         return 1;
       case 1:
         // Show the coming soon view
-        coming_soon_view(row, col);
-        main_menu_view(row, col);
+        // todo: remove arguments not needed
+        coming_soon_view();
+        main_menu_view();
         return 0;
       case 2:
         // show the help view
-        help_view(row, col);
-        main_menu_view(row, col);
+        help_view();
+        main_menu_view();
         return 0;
       case 3:
         // Leave the game
