7a49218c7973b2241c82e73d0707f3c7016a2b21
Ali-Brarou <alibrarou16@gmail.com>
2025-09-26 16:45:13 +0100

    [UPDATE] improve Makefile and fix function prototypes

--- src/helpers.h
+++ src/helpers.h
@@ -1,9 +1,11 @@
 #ifndef _HELPERS_H_
 #define _HELPERS_H_
 
+#include <curses.h>
+
 // Prototypes
-void exit_curses();
-void getch_exit_curses();
-void wreset();
+void exit_curses(int code);
+void getch_exit_curses(int code);
+void wreset(WINDOW *win);
 
 #endif
